de9dd6bfa9f00e23042b139dc24c49e26f2c33b0
Earl Tankard, Jr <theflashtank21@gmail.com>
2019-11-07 01:58:40 -0500

    Program now exits if user hits enter or if they reach the 1000 char limit

--- project2.s
+++ project2.s
@@ -17,12 +17,14 @@ main:
 
 	getInput:				#gets all of the input from a user until they hit '\n'
 		la $s0, array			#load address of array into $s0
+		move $s3, $zero			#counter to keep track of how many chars
 		loop:	
 			jal getChar		#jump to getChar
 			lb $t0, char		#load a single byte into $t0, remove null
 			sb $t0, 0($s0)		#store the char array
 			lb $t1, newline		#load '\n' into $t1
-			beq $t0, $t1, exit	#end of string?  jump to exit
+			beq $t0, $t1, exit	#end of string ? jump to exit
+			beq $s3, 1000, exit	#1000 chars ? jump to exit
 			addi $s0, $s0, 4	#$s0 += 4
 
 			j loop			#jumps back to start of loop
@@ -34,6 +36,8 @@ main:
 			la $a0, char	#load address of char for read
 			li $a1, 2	#length of string is 1byte char and 1byte for null
 			syscall
+			addi $s3, $s3, 1#adds 1 to $s3
+
 			jr $ra
 	
 	badChar:
